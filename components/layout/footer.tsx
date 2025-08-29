6930b67fdc1b54eaf152e529e3d64278bbcf7ffa
Anirudra Gupta <infinityservicesproviders12@gmail.com>
2025-08-29 19:48:27 +0530

    Update footer.tsx

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -40,12 +40,12 @@ export default async function Footer() {
         <div className="md:ml-auto">
           <a
             className="flex h-8 w-max flex-none items-center justify-center rounded-md border border-neutral-200 bg-white text-xs text-black dark:border-neutral-700 dark:bg-black dark:text-white"
-            aria-label="Deploy on Vercel"
-            href="https://vercel.com/templates/next.js/nextjs-commerce"
+            aria-label="Get your own on infinity services providers"
+            href="https://www.infinityservicesproviders.com/infinityservicesproviders-com"
           >
-            <span className="px-3">▲</span>
+            <span className="px-3"></span>
             <hr className="h-full border-r border-neutral-200 dark:border-neutral-700" />
-            <span className="px-3">Deploy</span>
+            <span className="px-3">Get your own on infinity services providers</span>
           </a>
         </div>
       </div>
@@ -57,11 +57,11 @@ export default async function Footer() {
           </p>
           <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
           <p>
-            <a href="https://github.com/vercel/commerce">View the source</a>
+            <a href="https://github.com/anirudra-gupta">View the source</a>
           </p>
           <p className="md:ml-auto">
             <a href="https://vercel.com" className="text-black dark:text-white">
-              Created by ▲ Vercel
+              Created by infinity services providers
             </a>
           </p>
         </div>
